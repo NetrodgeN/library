c4b4c544584c5e517a718cc39da67cdec9e1e149
Yarolsav <netrodgen@rambler.ru>
2022-04-30 15:06:43 +0300

    feat: add star rating

--- src/components/BookList/BookList.tsx
+++ src/components/BookList/BookList.tsx
@@ -4,6 +4,7 @@ import {fetchBook} from "../../store/action-creator/book";
 import {useTypeSelector} from "../../hooks/useTypeSelector";
 import classes from './BookList.module.css'
 import {IBook} from "../../types/book";
+import Rating from "../Rating/Rating";
 
 interface BookListProps {
     sortedAndSearchedBooks:IBook[];
@@ -28,7 +29,7 @@ const BookList: FC<BookListProps> = ({sortedAndSearchedBooks}) => {
                     <img className={classes.book__cover} src={book.image_url} alt="logo"/>
                     <div>{book.title}</div>
                     <div>by {book.author.firstName} {book.author.lastName}</div>
-                    <div>{book.rating}</div>
+                    <Rating props={book.rating}/>
                 </div>
             )}
         </div>
---
+++ src/components/Rating/Rating.tsx
@@ -0,0 +1,73 @@
+import React, {useState, FC} from 'react';
+import starImg from './star.png'
+
+interface IstarProps{
+    starId:number;
+    rating: number|null;
+    onMouseEnter: React.MouseEventHandler<HTMLDivElement>;
+    onMouseLeave: React.MouseEventHandler<HTMLDivElement>;
+    onClick: React.MouseEventHandler<HTMLDivElement>;
+}
+
+const Star: FC<IstarProps> = ({starId, rating, onMouseEnter, onMouseLeave, onClick}) => {
+    let styleClass = "star-rating-blank";
+    if (rating && rating >= starId) {
+        styleClass = "star-rating-filled";
+    }
+    return (
+        <div className='star'
+             onMouseEnter={onMouseEnter}
+             onMouseLeave={onMouseLeave}
+             onClick={onClick}
+        >
+            <img
+                height="16px"
+                className={styleClass}
+                src={starImg}
+                alt="star"
+            />
+        </div>
+    );
+};
+
+
+interface IRatingProps{
+    props:number;
+}
+
+const Rating: FC<IRatingProps> = (props) => {
+    const [selectedRate, setSelectedRate] = useState<null|number>(props.props)
+    const [hoveredRate, setHoveredRate] = useState<null|number>(null)
+    const stars =[1,2,3,4,5]
+
+    return (
+        <div className='rating__body'>
+            <div className="stars">
+                {stars.map((star) =>
+                    <div>
+                    <Star
+                        key ={star}
+                        rating={hoveredRate || selectedRate}
+                        onMouseLeave={()=> setHoveredRate(0)}
+                        onMouseEnter={()=>setHoveredRate(star)}
+                        onClick={()=>  {
+                            if (selectedRate === star){
+                                setSelectedRate(null)
+                            } else {
+                                setSelectedRate(star)
+                            }
+
+                        }
+                        }
+                        starId={star}
+                    />
+                    </div>
+
+
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default Rating;
